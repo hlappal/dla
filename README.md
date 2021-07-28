f05c6c7113a6b12ac143f0ce0c077aef299f0c16
Heikki Lappalainen <heikki.lappalainen@protonmail.com>
2021-07-28 19:05:21 +0300

    Update

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
-# Diffusion-limited aggregation algorithm
+# Diffusion-limited aggregation
 
-Diffusion-limited aggregation effectively describes a system where diffusion is the primary way of transportation, so that particles moving via Brownian motion cluster together forming aggregates, or Brownian trees ([Wikipedia.](https://en.wikipedia.org/wiki/Diffusion-limited_aggregation)). This program is an attempt to implement this algorithm in a simple 2D case.
+Diffusion-limited aggregation effectively describes a system where diffusion
+is the primary way of transportation, so that particles moving via Brownian
+motion cluster together forming aggregates, or Brownian trees
+([Wikipedia.](https://en.wikipedia.org/wiki/Diffusion-limited_aggregation)).
+This program implements the algorithm in simple 2D cases where the aggregate
+grows around a central seed or up from a surface.
 
 The algorithm:
   * Place a seed at the center of the lattice.
-  * Release a random walker from the edge.
+  * Release a random walker from the edge (or from the top in the surface
+  case).
   * Random walker sticks to the neighbouring sites of the seed/previous points.
   * Repeat N times.
 
-Run of N = 1000 on a 120x120 size lattice:
+Example of central DLA with 1000 random walkers on a 100x100 lattice:
 
-![Run of N = 1000 on a 120x120 size lattice.](/images/dla_dendrite_growth_N1000.png)
+![Central DLA with N = 1000 on a 100x100 size lattice.](/images/dla_dendrite_growth_N1000.png)
+
+## Usage
+
+Run the program from the command line with
+
+```
+dla.py [-h] [-p P] model num-walkers lattice
+
+positional arguments:
+  model        DLA growth type: [c]entral, [s]urface
+  num-walkers  Number of random walkers
+  lattice      Lattice side length
+  
+optional arguments:
+  -h, --help   Show this help message and exit
+  -p sticky    Sticky factor (default 1.0)
+```
+
+## To do
+
+The algorithm is not very optimized, as the walkers are released one at a time,
+and there are no limiting conditions (birth/death distance, etc.).
+
+The performance could be improved by:
+ * Implementing the main loop as a Numpy operation
+ * Introducing limiting conditions for the random walkers
--- dla.py
+++ dla.py
@@ -1,84 +1,176 @@
-# Diffusion-limited aggregation
-#
-# The algorithm:
-#  Place a seed at the center of the lattice.
-#  Release a random walker from the edge.
-#  Random walker sticks to the neighboring sites of the seed.
-#  Repeat N times, leads into dendrite growth.
-#
-# Code by Heikki Lappalainen
-# email: heikki.lappalainen@protonmail.com
-# Date: 30 Dec 2019
+#!/usr/bin/env python3
 
+"""
+Diffusion limited aggregation on central seed and on surface.
+
+Author: Heikki Lappalainen
+Email:  heikki.lappalainen@protonmail.com
+"""
+
+import sys
+import argparse
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+from tqdm import tqdm
 
-start = time.time()
-latt = [h, w] = [60, 60]    # height and width of the lattice
-N = 500                     # number of random walkers
-sticking_factor = 0.1       # sticking factor defines the probability of
-                            #  sticking during contact
 
-lattice = np.zeros((h, w)).astype(int)  # initialize empty lattice
-lattice[h//2, w//2] = 1                 # initial seed point
+class CentralDLA():
+    """
+    DLA simulation on central seed. Random walkers spawn on random points at
+    the lattice edges and perform Brownian walk until they hit a neighboring
+    site of previous fixed sites.
+    """
+    def __init__(self, n_walkers: int, L: int, p: float) -> None:
+        self.n_walkers = n_walkers                  # Number of random walkers
+        self.L = L                                  # Lattice side length
+        self.p = p                                  # Sticky factor in [0..1]
+        self.lattice = np.zeros((L, L), dtype=int)  # Simulation lattice
+        self.lattice[L//2, L//2] = 1                # Central seed
+        self.pos = np.zeros(2, dtype=int)           # Current walker position
+        # List the possible moves for random walkers: up, down, left, right
+        self.moves = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
 
-neighbors = []                          # the list of neighboring sites
-# add the initial neighboring sites
-neighbors.append([h//2 + 1, w//2])
-neighbors.append([h//2 - 1, w//2])
-neighbors.append([h//2, w//2 + 1])
-neighbors.append([h//2, w//2 - 1])
+    def init_walker(self) -> None:
+        """
+        Generate a new random walker on one of the four edges.
+        """
+        self.pos = np.array([np.random.randint(self.L),
+                             np.random.choice([0, self.L-1])])
 
-def randomWalk(pos, lattice):
-    """Perform a random walk from given point until contact"""
+    def pos_valid(self, pos: np.array) -> bool:
+        """
+        Check if the given position is free and inside the lattice.
 
-    while True:
-        i = np.random.randint(2)  # choose vertical or horizontal movement
-        pos[i] += np.random.randint(-1,2)  # choose direction and move
-        # prevent walker going out of the lattice
-        if pos[i] < 0:
-            pos[i] = 0
-            continue
-        if pos[i] >= latt[i]:
-            pos[i] -= 1
-            continue
-        # check for contact
-        if pos in neighbors:
-            # sticking occurs according to the sticking factor
-            if np.random.rand() < sticking_factor:
-                # add the neighboring sites to the list
-                for site in [[pos[0] + 1, pos[1]],
-                             [pos[0] - 1, pos[1]],
-                             [pos[0], pos[1] + 1],
-                             [pos[0], pos[1] - 1]]:
-                    if site not in neighbors:  # prevent double counting
-                        neighbors.append(site)
-                lattice[pos[0], pos[1]] = 1
-                return lattice
-            else:
-                continue
+        Params:
+          pos (np.array):  The position coordinates.
+        Returns:
+          (boolean):       True if the position is valid, False otherwise.
+        """
+        if (pos[0] >= 0 and pos[1] >= 0 and pos[0] < self.L and pos[1] < self.L
+                and self.lattice[pos[0], pos[1]] == 0):
+            return True
+        return False
 
-for i in range(N):
-    print('Random walker #{}'.format(i+1))
-    # choose the starting position of the random walker
-    pos = [x, y] = [np.random.randint(w), np.random.randint(h)]
-    i = np.random.randint(2)  # choose either the x or y axis
-    # the chosen axis assigned a value of either min or max to put it on
-    # the lattice edge
-    if np.random.rand() < 0.5:
-        pos[i] = 0
+    def move_walker(self) -> None:
+        """
+        Move walker into one of the allowed neighboring sites.
+        """
+        while True:
+            ind = np.random.randint(4)
+            move = self.moves[ind]     # Random move direction
+            new_pos = self.pos + move  # Trial move
+            if self.pos_valid(new_pos):
+                self.pos = new_pos     # Accept move
+                break
+
+    def on_sticky_site(self) -> None:
+        """
+        Determine if the walker is on a sticky site. For each adjacent
+        direction in the lattice, check if the adjacent cell value is
+        greater than zero (i.e. is occupied).
+
+        Returns:
+          (boolean):  True if on a sticky site, False otherwise.
+        """
+        inds = self.pos + self.moves
+        inds = inds[(inds[:, 0] > 0) & (inds[:, 0] < self.L)
+                    & (inds[:, 1] > 0) & (inds[:, 1] < self.L)]
+        if (self.lattice[inds[:, 0], inds[:, 1]].sum() > 0):
+            return True
+        return False
+
+    def run(self) -> None:
+        """
+        Run the simulation main loop.
+        """
+        start = time.time()
+
+        for i in tqdm(range(self.n_walkers)):
+            # Generate new walker
+            self.init_walker()
+
+            # Perform random walk until the walker hits a sticky site
+            while True:
+                # Check if the walker is on a sticky site
+                if self.on_sticky_site():
+                    # Fix the walker into the site with a running integer value
+                    self.lattice[self.pos[0], self.pos[1]] = i+1
+                    break
+
+                # Take a random step
+                self.move_walker()
+
+
+        print(f"Simulated {self.n_walkers} random walks on a {self.L}x{self.L}"
+              + f" lattice in {time.time() - start:.4f} seconds.")
+
+    def plot(self) -> None:
+        """
+        Plot the simulation results.
+        """
+        fig, ax = plt.subplots(figsize=(5, 5))
+        ax.imshow(self.lattice, cmap='magma')
+        plt.show()
+
+
+class SurfaceDLA(CentralDLA):
+    """
+    DLA growth on a surface. The random walkers spawn on the top edge.
+    The class inherits the CentralDLA class, but re-defines
+    """
+    def __init__(self, n_walkers: int, L: int, p=1.0) -> None:
+        CentralDLA.__init__(self, n_walkers, L, p)
+        self.lattice = np.zeros((L, L), dtype=int)  # Re-initialize the lattice
+        self.lattice[L-1, :] = 1  # Surface at the bottom of lattice
+
+    def init_walker(self) -> None:
+        """
+        Generate a new random walker on a random point at the top of the
+        lattice. The growth might reach the lattice top, so the site must be
+        checked.
+        """
+        self.pos[0] = 0
+        while True:
+            self.pos[1] = np.random.randint(self.L)
+            if self.pos_valid(self.pos):
+                break  # Accept valid position
+
+
+def main(argv: list) -> None:
+    """
+    Simulation main loop.
+    """
+    print(argv[0])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", metavar="model",
+                        help="DLA growth type: [c]entral, [s]urface")
+    parser.add_argument("walkers", metavar="num-walkers",
+                        help="Number of random walkers", type=int)
+    parser.add_argument("lattice", metavar="lattice",
+                        help="Lattice side length", type=int)
+    parser.add_argument("-p",
+                        help="Sticky factor (default 1.0)", type=float)
+
+    args = parser.parse_args()
+    n_walkers = args.walkers
+    L = args.lattice
+    if args.p:
+        p = args.p
     else:
-        pos[i] = latt[i]-1
-    # run random walk from the chosen site
-    lattice = randomWalk(pos, lattice)
+        p = 1.0
 
-stop =  time.time()
-print('Finished in {:.4f} seconds'.format(stop - start))
+    if args.model in ["c", "central"]:
+        dla = CentralDLA(n_walkers, L, p)
+    elif args.model in ["s", "surface"]:
+        dla = SurfaceDLA(n_walkers, L, p)
+    else:
+        parser.print_help()
+        sys.exit(1)
 
-# simple plot
-plt.matshow(lattice)
-plt.xticks(ticks=[], labels=None)
-plt.yticks(ticks=[], labels=None)
-plt.show()
+    dla.run()
+    dla.plot()
 
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
